984a34688428e9de46ac4a6b6bc9993963aa7623
milandvic <milandvic@gmail.com>
2026-08-26 21:01:57 +0200

    MainMenu initialization

--- src/game.hpp
+++ src/game.hpp
@@ -1,32 +1,61 @@
 #pragma once
 
+struct Game;
 
-
-struct Game
+struct GameState
 {
+    void (*OnUpdate)(Game& g, float dt);
+    void (*OnDraw)(const Game& g, float dt);
+};
 
-
+struct MainMenu
+{
 
 };
 
+struct Game
+{
+    GameState *state;
+    MainMenu mMainMenu;
 
+};
 
+inline void MainMenu_Init(MainMenu& m)
+{
+
+}
+
+inline void MainMenu_OnUpdate(Game& g, float dt)
+{
+
+}
+
+inline void MainMenu_OnDraw(const Game& g, float dt)
+{
+
+}
+
+GameState gMainMenuState = {
+    .OnUpdate = MainMenu_OnUpdate,
+    .OnDraw = MainMenu_OnDraw,
+};
 
 inline void Game_Init(Game& g)
 {
-
+    MainMenu_Init(g.mMainMenu);
+    g.state = &gMainMenuState;
 
 }
 
 inline void Game_Update(Game& g)
 {
-
+    g.state->OnUpdate(g, GetFrameTime());
 
 }
 
 inline void Game_Draw(const Game& g)
 {
-
+    g.state->OnDraw(g, GetFrameTime());
 }
 
 inline void Game_Shutdown(Game& g)
